a98a67bcecd9632936835306aad19e8c35ab3238
Shahad It <shahadalrehily@gmail.com>
2025-09-01 12:51:17 +0000

    Solved the first challenge of the bootcamp :)

--- demo.py
+++ demo.py
@@ -2,6 +2,14 @@
 
 
 
+import math
+
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    """
+    Returns the area of the circle of given radius.
+    If the radius is negative, return 0.
+    """
+    if radius > 0:
+        return radius * radius * math.pi
+    else:
+        return 0
